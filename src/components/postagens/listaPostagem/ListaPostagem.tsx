e3910fc7a194c53a490a53230caf01fc5818c2fc
nancidama <nanci.damasceno@sumup.com>
2022-03-08 10:40:38 -0300

    Inclusão de ListaPostagem

---
+++ src/components/postagens/listaPostagem/ListaPostagem.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem';
+import { busca } from '../../../service/Service'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import './ListaPostagem.css';
+import { useHistory } from 'react-router-dom'
+// import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import useLocalStorage from 'react-use-localstorage';
+
+
+function ListaPostagem() {
+    const [posts, setPosts] = useState<Postagem[]>([])
+    const [token, setToken] = useLocalStorage('token')
+    let history = useHistory();
+    // const token = useSelector<TokenState, TokenState["tokens"]>(
+    //     (state)=> state.tokens
+    // );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.error('Você precisar estar logado para prosseguir', {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            history.push("/login")
+
+        }
+    }, [token])
+
+    async function getPost() {
+        await busca("/postagem", setPosts, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+
+        getPost()
+
+    }, [posts.length])
+
+    return (
+        <>
+            {
+                posts.map(post => (
+                    <Box m={2} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Postagens
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {post.titulo}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {post.texto}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {post.tema?.tema}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                                    <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+                                        <Box mx={1}>
+                                        <Button variant="contained" size='small' className="marginLeft1" color='primary'>
+                                                Atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                        <Button variant="contained" size='small' className="marginLeft1" color='secondary'>
+                                                Deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                </Box>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))
+            }
+        </>
+    )
+}
+
+export default ListaPostagem;
